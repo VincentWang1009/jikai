5d1b6009c97bf5048e26d09aa0c2f0e70631cf5a
vincentwang1009 <vincent.wang610@gmail.com>
2015-03-14 12:41:25 +0800

    mod 7

--- js/backtop.js
+++ js/backtop.js
@@ -3,4 +3,14 @@ $(function(){
         $('body,html').animate({scrollTop:0},300);
     });
 
+    $(window).scroll(function(){
+        if($(this).scrollTop()>=100) {
+            $('.jikai-backtop').fadeIn();
+        }
+        if($(this).scrollTop()<100) {
+            $('.jikai-backtop').fadeOut();
+        }
+    });
+
+
 });
